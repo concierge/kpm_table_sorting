013a492d810b26a98fc0a96168fa6aa1cb8055a9
p0358 <p0358@users.noreply.github.com>
2017-08-11 20:03:49 +0200

    Don't save file if no changes were made

--- sort.php
+++ sort.php
@@ -2,18 +2,21 @@
 
 $filename = $argv[1] ?? null;
 
-if (empty($filename)) die('Filename not provided!');
+if (empty($filename))
+    die('Filename not provided!');
 
-$file = file_get_contents($filename);
+$file_contents = file_get_contents($filename);
 
-$split = explode("|---|---|\n", $file);
+$split = explode("|---|---|\n", $file_contents);
 $tablestring = $split[1];
 
-if (empty($tablestring)) die('Error, empty tablestring!');
+if (empty($tablestring))
+    die('Error, empty tablestring!');
 
-$modules_arr = explode("\n", $tablestring);
+$old_modules_arr = $modules_arr = explode("\n", $tablestring);
 
 sort($modules_arr, SORT_STRING);
 
 $newtable = $split[0] . "|---|---|\n" . implode("\n", $modules_arr);
-file_put_contents($argv[1], $newtable);
+if ($newtable != $file_contents && $modules_array != $old_modules_arr)
+    file_put_contents($argv[1], $newtable);
